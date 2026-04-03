3d83c36d0272754fbbe00fd992859ce901848d87
Ruan Pablo <ruanpablomota23@gmail.com>
2026-04-02 21:39:51 -0300

    feat: adiciona calculo de idade com LocalDate

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,2 +1,21 @@
+import java.time.LocalDate;
+import  java.time.Period;
+
 public class Aluno {
+    String nome;
+    int idade;
+    private LocalDate dataNacimento;
+
+    public void apresentar(){
+        System.out.println("Ola, meu nome é " + nome + " e tenho " + calcularIdade() + " anos.");
+    }
+
+    public Aluno(String nome, LocalDate dataNacimento) {
+        this.nome = nome;
+        this.dataNacimento = dataNacimento;
+    }
+
+    public int calcularIdade () {
+        return Period.between(dataNacimento, LocalDate.now()).getYears();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Aluno a = new Aluno("Alana", LocalDate.of(2000, 7,15));
+
+        System.out.println(a.calcularIdade());
+        a.apresentar();
+    }
 }
