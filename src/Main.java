945cfac51f87dbe5f4fa3b5a55038b74d5ef354e
Ruan Pablo <ruanpablomota23@gmail.com>
2026-04-02 22:11:58 -0300

    feat: implementa cadastro de alunos com Scanner e ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,42 @@
 import java.time.LocalDate;
+import java.util.Scanner;
+import  java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        Aluno a = new Aluno("Alana", LocalDate.of(2000, 7,15));
+    static Scanner sc = new Scanner(System.in);
 
-        System.out.println(a.calcularIdade());
-        a.apresentar();
+
+
+    public static void main(String[] args) {
+        System.out.println("1 - Cadastrar aluno");
+        System.out.println("2 - Sair");
+         int opcao = sc.nextInt();
+
+        ArrayList<Aluno> alunos = new ArrayList<>();
+
+        if (opcao == 1) {
+            sc.nextLine(); // limpar buffer
+
+            System.out.println("Digite o nome:");
+            String nome = sc.nextLine();
+
+            System.out.println("Digite o ano de nascimento:");
+            int ano = sc.nextInt();
+
+            System.out.println("Digite o mes:");
+            int mes = sc.nextInt();
+
+            System.out.println("Digite o dia:");
+            int dia = sc.nextInt();
+
+            Aluno aluno = new Aluno(nome, LocalDate.of(ano, mes, dia));
+            alunos.add(aluno);
+
+            System.out.println("Aluno cadastrado!");
+        }
+        for (Aluno a : alunos) {
+            a.apresentar();
+        }
     }
+
 }
