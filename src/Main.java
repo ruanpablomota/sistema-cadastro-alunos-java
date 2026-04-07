3b05d2192adc378398aab264525eb8de9cabcd5f
Ruan Pablo <ruanpablomota23@gmail.com>
2026-04-06 22:07:05 -0300

    feat: adiciona listagem de alunos e loop no menu

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,59 @@ import  java.util.ArrayList;
 public class Main {
     static Scanner sc = new Scanner(System.in);
 
-
-
     public static void main(String[] args) {
-        System.out.println("1 - Cadastrar aluno");
-        System.out.println("2 - Sair");
-         int opcao = sc.nextInt();
 
         ArrayList<Aluno> alunos = new ArrayList<>();
 
-        if (opcao == 1) {
-            sc.nextLine(); // limpar buffer
+        while (true) {
+            //menu aqui
+            System.out.println("1 - Cadastrar aluno");
+            System.out.println("2 - Listar alunos");
+            System.out.println("3 - Sair");
 
-            System.out.println("Digite o nome:");
-            String nome = sc.nextLine();
+            int opcao = sc.nextInt();
 
-            System.out.println("Digite o ano de nascimento:");
-            int ano = sc.nextInt();
+            if (opcao == 1) {
+                sc.nextLine(); // limpar buffer
 
-            System.out.println("Digite o mes:");
-            int mes = sc.nextInt();
+                System.out.println("Digite o nome:");
+                String nome = sc.nextLine();
 
-            System.out.println("Digite o dia:");
-            int dia = sc.nextInt();
+                System.out.println("Digite o ano de nascimento:");
+                int ano = sc.nextInt();
 
-            Aluno aluno = new Aluno(nome, LocalDate.of(ano, mes, dia));
-            alunos.add(aluno);
+                System.out.println("Digite o mes:");
+                int mes = sc.nextInt();
 
-            System.out.println("Aluno cadastrado!");
+                System.out.println("Digite o dia:");
+                int dia = sc.nextInt();
+
+                Aluno aluno = new Aluno(nome, LocalDate.of(ano, mes, dia));
+                alunos.add(aluno);
+
+                System.out.println("Aluno cadastrado!");
+            }
+
+            else if (opcao == 2) {
+                if (alunos.isEmpty()) {
+                    System.out.println("Nenhum aluno castrado.");
+                } else {
+                    for (Aluno a : alunos) {
+                        a.apresentar();
+                    }
+                }
+            }
+
+            else if (opcao == 3) {
+                System.out.println("Saindo...");
+                break;
+            }
+
+            else {
+                System.out.println("Opção inválida");
+            }
         }
-        for (Aluno a : alunos) {
-            a.apresentar();
-        }
+
     }
 
 }
